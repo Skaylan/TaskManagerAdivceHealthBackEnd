5b85f8b7431d934b56437a49711a8aaf7a09cbe0
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-17 09:46:53 -0300

    Feat: Add User model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password_hash = models.TextField()
+    created_at = models.DateTimeField(editable=False, default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
