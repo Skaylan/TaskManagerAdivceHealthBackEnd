5bf3ce310b74b6f752f818c2f13731148d406edd
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-17 09:47:20 -0300

    Feat: Add Task model

--- api/models.py
+++ api/models.py
@@ -10,3 +10,13 @@ class User(models.Model):
     password_hash = models.TextField()
     created_at = models.DateTimeField(editable=False, default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
+
+
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_done = models.BooleanField(default=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(editable=False, default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
