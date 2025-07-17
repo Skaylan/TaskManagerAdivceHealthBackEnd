53c059c1b959fad8e88436a56e972a7105a258ae
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-17 09:53:49 -0300

    Feat: Add API endpoint to retrieve all users and configure URL routing

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer, TaskSerializer
+from .models import User, Task
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_all_users(request):
+    try:
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        return Response({'error': 'Internal Server Error'}, status=500)
