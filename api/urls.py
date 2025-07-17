c9207f44c96271ebd5257a432280ae552d94f3d5
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-17 20:15:16 -0300

    Feat: Implement authentication endpoint with email and password validation

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 # from . import views
 from api.controllers.user_controller import UserController
 from api.controllers.task_controller import TaskController
+from api.controllers.auth_controller import AuthController
+
 urlpatterns = [
     path('get_all_users', UserController.get_all_users, name='get_all_users'),
     path('get_user_by_email', UserController.get_user_by_email, name='get_user_by_email'),
@@ -10,4 +12,5 @@ urlpatterns = [
     path('delete_task', TaskController.delete_task, name='delete_task'),
     path('update_task_status', TaskController.update_task_status, name='update_task_status'),
     path('update_task_infos', TaskController.update_task_infos, name='update_task_infos'),
+    path('authenticate', AuthController.authenticate, name='authenticate'),
 ]
