c0d932151d2ba16047b4f2b5c90a00da959b5433
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-17 09:56:44 -0300

    Feat: Implement API endpoint to retrieve user by email with error handling

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .serializers import UserSerializer, TaskSerializer
 from .models import User, Task
 from django.contrib.auth.hashers import make_password, check_password
+from core.utils import print_error_details
 
 # Create your views here.
 
@@ -13,4 +14,25 @@ def get_all_users(request):
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
     except Exception as e:
+        print_error_details(e)
+        return Response({'error': 'Internal Server Error'}, status=500)
+
+@api_view(['GET'])
+def get_user_by_email(request):
+    email = request.GET.get('email')
+
+    if email is None or email == '':
+        return Response({'error': 'Email is required'}, status=400)
+
+    try:
+        User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+
+    try:
+        user = User.objects.get(email=email)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    except Exception as e:
+        print_error_details(e)
         return Response({'error': 'Internal Server Error'}, status=500)
