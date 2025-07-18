0a0167c0f61cb359b5e3326980354a7390791fd6
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-18 12:24:34 -0300

    feat: add user to auth response

--- api/controllers/auth_controller.py
+++ api/controllers/auth_controller.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from core.utils import print_error_details
 from django.contrib.auth.hashers import check_password
 from api.services.user_service import UserService
+from api.serializers import UserSerializer
 import jwt
 from dotenv import load_dotenv
 
@@ -24,12 +25,19 @@ class AuthController:
                 return Response({'error': 'Password is required'}, status=status.HTTP_400_BAD_REQUEST)
 
             user = UserService.get_user_by_email(data.get('email'))
-
             if not check_password(data.get('password'), user.password_hash):
                 return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
+            serializer = UserSerializer(user)
             token = jwt.encode({'email': user.email}, os.getenv('SECRET_KEY'), algorithm='HS256')
-            return Response({'message': 'Authenticated', 'token': token}, status=status.HTTP_200_OK)
+            return Response(
+                {
+                    'message': 'Authenticated',
+                    'token': token,
+                    'user': serializer.data
+                },
+                status=status.HTTP_200_OK
+            )
         except Exception as e:
             print_error_details(e)
             return Response({'error': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
