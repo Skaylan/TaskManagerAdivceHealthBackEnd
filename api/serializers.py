ee1f19aa6d0e45f8bfc65128d902cf8ad12a26b4
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-19 18:48:16 -0300

    feat: add category serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from api.models.user import User
 from api.models.task import Task
-
+from api.models.category import Category
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,7 +10,15 @@ class UserSerializer(serializers.ModelSerializer):
         exclude = ['password_hash']
 
 
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
 class TaskSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=True, read_only=True)
     class Meta:
         model = Task
         fields = '__all__'
