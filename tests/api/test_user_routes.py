0a362c35d3e9c3fc0e88bab92cde979e9f6ead09
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-19 14:39:17 -0300

    tests: add test to user routes

--- tests/api/test_user_routes.py
+++ tests/api/test_user_routes.py
@@ -2,11 +2,29 @@ import pytest
 from rest_framework.test import APIClient
 from tests.helpers import *
 
-
 client = APIClient()
-
 
 @pytest.mark.django_db
 def test_create_user():
-    user_response = create_user(client)
+    user_response = create_user(client, CREATE_USER_PAYLOAD)
     assert user_response.status_code == 201
+
+
+@pytest.mark.django_db
+def test_get_all_users():
+    user_response = create_user(client, CREATE_USER_PAYLOAD)
+    assert user_response.status_code == 201
+
+    user_response = get_all_users(client)
+    assert user_response.status_code == 200
+    assert user_response.data[0]['email'] == CREATE_USER_PAYLOAD['email']
+
+
+@pytest.mark.django_db
+def test_get_user_by_email():
+    user_response = create_user(client, CREATE_USER_PAYLOAD)
+    assert user_response.status_code == 201
+
+    user_response = get_user_by_email(client, CREATE_USER_PAYLOAD['email'])
+    assert user_response.status_code == 200
+    assert user_response.data['email'] == CREATE_USER_PAYLOAD['email']
--- tests/helpers.py
+++ tests/helpers.py
@@ -3,11 +3,23 @@ ENDPOINT = 'http://localhost:8000/api/v1'
 
 CREATE_USER_PAYLOAD = {
     'name': 'Test User',
-    'email': '5V8l2@example.com',
+    'email': 'test@example.com',
     'password': 'password123',
     're_password': 'password123'
 }
 
+ADD_TASK_PAYLOAD = {
+    'title': 'Tarefa 1',
+    'description': 'Descrição da tarefa 1',
+    'user_id': 1
+}
 
-def create_user(client):
-    return client.post(f'{ENDPOINT}/create_user', CREATE_USER_PAYLOAD, format='json')
+def create_user(client, payload: dict):
+    return client.post(f'{ENDPOINT}/create_user', payload, format='json')
+
+def get_all_users(client):
+    return client.get(f'{ENDPOINT}/get_all_users')
+
+def get_user_by_email(client, email: str):
+    return client.get(f'{ENDPOINT}/get_user_by_email?email={email}')
+
