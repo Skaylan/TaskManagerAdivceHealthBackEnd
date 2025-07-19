36bf2d611272d8e85da29f9d7a0d5dbf124d2ce3
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-19 14:41:11 -0300

    feat: return the task in response when creating a task

--- api/controllers/task_controller.py
+++ api/controllers/task_controller.py
@@ -26,7 +26,14 @@ class TaskController:
             if not task:
                 return Response({'error': 'Task not created!'}, status=status.HTTP_400_BAD_REQUEST)
 
-            return Response({'message': 'Task successfully created!'}, status=status.HTTP_201_CREATED)
+
+            serializer = TaskSerializer(task)
+            return Response(
+                {
+                    'message': 'Task successfully created!',
+                    'task': serializer.data
+                }, status=status.HTTP_201_CREATED
+            )
         except Exception as e:
             print_error_details(e)
             return Response({'error': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
