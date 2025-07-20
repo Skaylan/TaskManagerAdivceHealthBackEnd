37e3bf6dd2202422328ce220e14ab56b38b1a40c
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-19 22:52:13 -0300

    refactor: add id and color in category model

--- api/models/category.py
+++ api/models/category.py
@@ -3,7 +3,9 @@ from django.utils import timezone
 from api.models.user import User
 
 class Category(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
+    color = models.CharField(max_length=10, default='#8B5CF6')
     created_at = models.DateTimeField(editable=False, default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
