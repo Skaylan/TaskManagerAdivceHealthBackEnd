75e4128b153340a6d84514bf7453c5c19c06e6cf
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-19 22:57:01 -0300

    feat: add update_category repository

--- api/repositories/category_repository.py
+++ api/repositories/category_repository.py
@@ -1,11 +1,14 @@
 from api.models.category import Category
 from core.utils import print_error_details
 from api.models.user import User
+from api.models.task import Task
+
+
 class CategoryRepository:
     @staticmethod
-    def create_category(name: str, user: User) -> Category:
+    def create_category(name: str, user: User, color: str) -> Category:
         try:
-            new_category = Category(name=name, user=user)
+            new_category = Category(name=name, user=user, color=color)
             new_category.save()
             return new_category
         except Exception as e:
@@ -40,3 +43,14 @@ class CategoryRepository:
         except Exception as e:
             print_error_details(e)
 
+    @staticmethod
+    def update_category(category_id: int, name: str, color: str) -> Category:
+        try:
+            category = Category.objects.get(id=category_id)
+            category.name = name
+            category.color = color
+            category.save()
+
+            return category
+        except Exception as e:
+            print_error_details(e)
