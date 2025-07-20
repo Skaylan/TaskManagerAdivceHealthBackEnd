8c798f159556a31b742cdbaace5eb29c878e8956
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-20 19:40:50 -0300

    docs: add documentation to test functions

--- tests/api/test_auth_routes.py
+++ tests/api/test_auth_routes.py
@@ -7,6 +7,12 @@ client = APIClient()
 
 @pytest.mark.django_db
 def test_authenticate():
+    """
+    Test the authenticate route.
+
+    This test creates a user, then tries to authenticate with it.
+    It checks that the returned user has the same email as the one created, and that the token is not empty.
+    """
     user_response = create_user(client, CREATE_USER_PAYLOAD)
     assert user_response.status_code == 201
 
